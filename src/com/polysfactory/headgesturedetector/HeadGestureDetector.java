e9993e2955d53b60c72f8f7cf5f5d9f4393e26fa
Poly Takahiro Horikawa <horikawa.takahiro@gmail.com>
2013-11-22 23:16:51 -0800

    Improve head shake and nod detection

--- src/com/polysfactory/headgesturedetector/HeadGestureDetector.java
+++ src/com/polysfactory/headgesturedetector/HeadGestureDetector.java
@@ -1,6 +1,5 @@
 package com.polysfactory.headgesturedetector;
 
-import java.util.Arrays;
 import java.util.List;
 
 import android.content.Context;
@@ -19,11 +18,11 @@ public class HeadGestureDetector implements SensorEventListener {
     private float[] orientationValues = new float[3];
     private float[] magneticValues = new float[3];
     private float[] accelerometerValues = new float[3];
+    private float[] orientationVelocity = new float[3];
 
     private SensorManager mSensorManager;
 
     private OnHeadGestureListener mListener;
-    private OrientationEvent mPreviousOrientationEvent;
 
     static enum State {
         IDLE, SHAKE_TO_RIGHT, SHAKE_BACK_TO_LEFT, SHAKE_TO_LEFT, SHAKE_BACK_TO_RIGHT, GO_DOWN, BACK_UP, GO_UP, BACK_DOWN
@@ -34,30 +33,21 @@ public class HeadGestureDetector implements SensorEventListener {
     private static final long STATE_TIMEOUT_NSEC = 1000 * 1000 * 1000;
     private static final int SENSOR_RATE = SensorManager.SENSOR_DELAY_GAME;
 
-    private static class OrientationEvent {
-        private float[] orientationValues;
-        private long timestamp;
-
-        private OrientationEvent(float[] orientationValues, long timestamp) {
-            this.orientationValues = orientationValues;
-            this.timestamp = timestamp;
-        }
-    }
-
     public HeadGestureDetector(Context context) {
         mSensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
     }
 
+    private static final int[] REQUIRED_SENSORS = { Sensor.TYPE_MAGNETIC_FIELD, Sensor.TYPE_ACCELEROMETER,
+            Sensor.TYPE_GYROSCOPE };
+
     public void start() {
         List<Sensor> sensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
-
         for (Sensor sensor : sensors) {
-            if (sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
-                mSensorManager.registerListener(this, sensor, SENSOR_RATE);
-            }
-
-            if (sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-                mSensorManager.registerListener(this, sensor, SENSOR_RATE);
+            for (int sensorType : REQUIRED_SENSORS) {
+                if (sensor.getType() == sensorType) {
+                    mSensorManager.registerListener(this, sensor, SENSOR_RATE);
+                    break;
+                }
             }
         }
     }
@@ -84,18 +74,21 @@ public class HeadGestureDetector implements SensorEventListener {
         }
 
         int sensorType = event.sensor.getType();
-        if (sensorType == Sensor.TYPE_MAGNETIC_FIELD) {
-            magneticValues = event.values.clone();
+
+        if (sensorType == Sensor.TYPE_GYROSCOPE) {
+            orientationVelocity = event.values.clone();
             return;
         }
 
-        // If sensorType is not TYPE_MAGNETIC_FIELD, then it is surely
-        // TYPE_ACCELEROMETER.
-        accelerometerValues = event.values.clone();
-
-        if (magneticValues == null || accelerometerValues == null) {
+        if (sensorType == Sensor.TYPE_ACCELEROMETER) {
+            accelerometerValues = event.values.clone();
+            SensorManager.getRotationMatrix(inR, I, accelerometerValues, magneticValues);
+            SensorManager.remapCoordinateSystem(inR, SensorManager.AXIS_X, SensorManager.AXIS_Z, outR);
+            SensorManager.getOrientation(outR, orientationValues);
             return;
         }
+
+        magneticValues = event.values.clone();
 
         // state timeout check
         if (event.timestamp - mLastStateChanged > STATE_TIMEOUT_NSEC && mState != State.IDLE) {
@@ -104,24 +97,8 @@ public class HeadGestureDetector implements SensorEventListener {
             mState = State.IDLE;
         }
 
-        SensorManager.getRotationMatrix(inR, I, accelerometerValues, magneticValues);
-        SensorManager.remapCoordinateSystem(inR, SensorManager.AXIS_X, SensorManager.AXIS_Z, outR);
-        SensorManager.getOrientation(outR, orientationValues);
-
         if (BuildConfig.DEBUG) {
             // Log.d(Constants.TAG, Arrays.toString(orientationValues));
-        }
-
-        float[] orientationVelocity = null;
-        OrientationEvent currentOrientationEvent = new OrientationEvent(orientationValues.clone(), event.timestamp);
-        if (mPreviousOrientationEvent == null) {
-            mPreviousOrientationEvent = currentOrientationEvent;
-            return;
-        }
-
-        // calc orientation velocity
-        orientationVelocity = getOrientationVelocity(mPreviousOrientationEvent, currentOrientationEvent);
-        if (BuildConfig.DEBUG) {
             // Log.d(Constants.TAG, "V:" + Arrays.toString(orientationVelocity));
         }
 
@@ -135,84 +112,67 @@ public class HeadGestureDetector implements SensorEventListener {
         int maxVelocityIndex = maxAbsIndex(orientationVelocity);
         if (isStable(orientationValues, orientationVelocity)) {
             // Log.d(Constants.TAG, "isStable");
-        } else if (maxVelocityIndex == 1) {
-            if (orientationVelocity[1] > minShakeSpeed) {
+        } else if (maxVelocityIndex == 0) {
+            if (orientationVelocity[0] < -MIN_MOVE_ANGULAR_VELOCITY) {
                 if (mState == State.IDLE) {
                     Log.d(Constants.TAG, "isNod");
                     mState = State.GO_DOWN;
                     mLastStateChanged = event.timestamp;
+                    if (mListener != null) {
+                        mListener.onNod();
+                    }
                 }
             }
-        } else if (maxVelocityIndex == 0) {
-            if (orientationVelocity[0] > minShakeSpeed) {
+        } else if (maxVelocityIndex == 1) {
+            if (orientationVelocity[1] < -MIN_MOVE_ANGULAR_VELOCITY) {
                 if (mState == State.IDLE) {
-                    Log.d(Constants.TAG, Arrays.toString(orientationValues));
-                    Log.d(Constants.TAG, "V:" + Arrays.toString(orientationVelocity));
-                    Log.d(Constants.TAG, "ShakingToRight");
+                    // Log.d(Constants.TAG, Arrays.toString(orientationValues));
+                    // Log.d(Constants.TAG, "V:" + Arrays.toString(orientationVelocity));
                     mState = State.SHAKE_TO_RIGHT;
                     mLastStateChanged = event.timestamp;
+                    if (mListener != null) {
+                        mListener.onShakeToRight();
+                    }
                 }
-            } else if (orientationVelocity[0] < -minShakeSpeed) {
+            } else if (orientationVelocity[1] > MIN_MOVE_ANGULAR_VELOCITY) {
                 if (mState == State.IDLE) {
-                    Log.d(Constants.TAG, Arrays.toString(orientationValues));
-                    Log.d(Constants.TAG, "V:" + Arrays.toString(orientationVelocity));
-                    Log.d(Constants.TAG, "ShakingToLeft");
+                    // Log.d(Constants.TAG, Arrays.toString(orientationValues));
+                    // Log.d(Constants.TAG, "V:" + Arrays.toString(orientationVelocity));
                     mState = State.SHAKE_TO_LEFT;
                     mLastStateChanged = event.timestamp;
+                    if (mListener != null) {
+                        mListener.onShakeToLeft();
+                    }
                 }
             }
         }
-
-        mPreviousOrientationEvent = currentOrientationEvent;
     }
 
-    private static final float minShakeSpeed = 0.01F;
+    private static final float MIN_MOVE_ANGULAR_VELOCITY = 1.00F;
 
-    private static final float maxStableRadian = 0.10F;
+    private static final float MAX_STABLE_RADIAN = 0.10F;
 
-    private static final float maxPutOnPitchRadian = 0.45F;
+    private static final float MAX_PUT_ON_PITCH_RADIAN = 0.45F;
 
-    private static final float maxPutOnRollRadian = 0.75F;
+    private static final float MAX_PUT_ON_ROLL_RADIAN = 0.75F;
 
-    private static final float STABLE_ORIENTATION_VELOCITY = 0.0005F;
+    private static final float STABLE_ANGULAR_VELOCITY = 0.10F;
 
     private static boolean isStable(float[] orientationValues, float[] orientationVelocity) {
-        if (Math.abs(orientationValues[1]) < maxStableRadian
-                && Math.abs(orientationVelocity[0]) < STABLE_ORIENTATION_VELOCITY
-                && Math.abs(orientationVelocity[1]) < STABLE_ORIENTATION_VELOCITY
-                && Math.abs(orientationVelocity[2]) < STABLE_ORIENTATION_VELOCITY) {
+        if (Math.abs(orientationValues[1]) < MAX_STABLE_RADIAN
+                && Math.abs(orientationVelocity[0]) < STABLE_ANGULAR_VELOCITY
+                && Math.abs(orientationVelocity[1]) < STABLE_ANGULAR_VELOCITY
+                && Math.abs(orientationVelocity[2]) < STABLE_ANGULAR_VELOCITY) {
             return true;
         }
         return false;
     }
 
     private static boolean isPutOn(float[] orientationValues, float[] orientationVelocity) {
-        if (orientationValues[1] < maxPutOnPitchRadian && Math.abs(orientationValues[2]) < maxPutOnRollRadian) {
+        if (orientationValues[1] < MAX_PUT_ON_PITCH_RADIAN && Math.abs(orientationValues[2]) < MAX_PUT_ON_ROLL_RADIAN) {
             return true;
         }
         return false;
-    }
-
-    private static float[] getOrientationVelocity(OrientationEvent e1, OrientationEvent e2) {
-        if (e1.timestamp > e2.timestamp) {
-            return getOrientationVelocity(e2, e1);
-        }
-
-        long diff = e2.timestamp - e1.timestamp;
-        diff /= 1000000; // msec
-        if (diff == 0) {
-            if (BuildConfig.DEBUG) {
-                Log.d(Constants.TAG, "Time is too close: " + e1.timestamp + ", " + e2.timestamp);
-            }
-            return null;
-        }
-
-        float[] velocity = new float[3];
-        for (int i = 0; i < 3; i++) {
-            // TODO normalize orientation value (what if e1=3.1 and e2=-3.1?)
-            velocity[i] = (e2.orientationValues[i] - e1.orientationValues[i]) / diff;
-        }
-        return velocity;
     }
 
     private static int maxAbsIndex(float[] array) {
